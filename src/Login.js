761b8bc169f9013e810379d91535e023058effa8
AbdussamadYisau <sammieyisau@gmail.com>
2022-04-18 23:14:19 +0100

    All done

--- src/Login.js
+++ src/Login.js
@@ -1,37 +1,77 @@
 import React, { useState } from "react";
-// JS
-// const input = document.getElementById('myText');
-// const inputValue = input.value
-// React
-// value, onChange
+import axios from "axios";
 
 const ControlledInputs = () => {
-  const [person, setPerson] = useState({ firstName: "", password: "", email: "", phone: "" });
-  const [people, setPeople] = useState([]);
+  const redirect = () => {
+    window.location.href = "/new";
+  };
+  const [person, setPerson] = useState({
+    password: "",
+    email: "",
+  });
+  const [people, setPeople] = useState({});
+  const [successStatus, setSucessStatus] = useState(false);
+  const [responseMessage, setResponseMessage] = useState("");
   const handleChange = (e) => {
     const name = e.target.name;
     const value = e.target.value;
     setPerson({ ...person, [name]: value });
   };
+
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (person.firstName && person.password && person.email&& person.phone) {
-      const newPerson = { ...person, id: new Date().getTime().toString() };
-      setPeople([...people, newPerson]);
-      setPerson({ firstName: "", password: "", email: "" , phone: ""});
-    }
+    // if ( person.password && person.email ) {
+
+    axios
+      .post("http://ttmg-backend.herokuapp.com/api/auth/staffLogin", {
+        email: person.email,
+        password: person.password,
+      })
+      .then((res) => {
+        console.log(res);
+        if (res.status === 200) {
+          setPeople(person);
+          console.log("People", people);
+          setSucessStatus(true);
+          setResponseMessage("Login is successful");
+          setPerson({ password: "", email: "" });
+          redirect();
+        }
+      })
+      .catch((err) => {
+        console.log("400", Object.keys(err), err.response, typeof err);
+        if (err.response.status === 400) {
+          setSucessStatus(true);
+          setResponseMessage("Email/Password is Missing");
+        }
+
+        if (err.response.status === 401) {
+          setSucessStatus(true);
+          setResponseMessage("Email or password is incorrect");
+        }
+      });
+    // }
   };
   return (
     <>
       <article className="form">
         <form>
+          {successStatus ? (
+            <div className="success">
+              {/* {`${people.email} has logged in successfully`} */}
+              {responseMessage}
+            </div>
+          ) : (
+            <></>
+          )}
+
           <div className="form-control">
             <input
-              type="text"
-              placeholder="Name"
-              id="firstName"
-              name="firstName"
-              value={person.firstName}
+              type="email"
+              placeholder="Email"
+              id="email"
+              name="email"
+              value={person.email}
               onChange={handleChange}
               required
             />
@@ -47,30 +87,8 @@ const ControlledInputs = () => {
               required
             />
           </div>
-          <div className="form-control">
-            <input
-              type="email"
-              placeholder="Email"
-              id="email"
-              name="email"
-              value={person.email}
-              onChange={handleChange}
-              required
-            />
-          </div>
-          <div className="form-control">
-            <input
-              type="number"
-              placeholder="Phone Number"
-              id="phone"
-              name="phone"
-              value={person.phone}
-              onChange={handleChange}
-              required
-            />
-          </div>
+
           <p>Forgot Password?</p>
-          <p>No account? <span>Sign Up</span></p>
           <button type="submit" className="btn" onClick={handleSubmit}>
             Login
           </button>
---
+++ src/NewPage.js
@@ -0,0 +1,9 @@
+import React from "react";
+const ControlledInputs = () => {
+  return (
+    <>
+    </>
+  );
+};
+
+export default ControlledInputs;
